a2b7c47193124e8cf0ad37a2b7db65bdcb008ff8
haydn5 <haydn@nautilusdesigns.com>
2025-09-20 11:45:22 -0400

    update metadata

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+  useSeoMeta({
+    title: "Savannah's Floatin' Party Starts Here | The Dancin Dolphin Cruise Company",
+    // First Tier Title | 2nd tier
+    description: "This is the description of the page you are viewing.",
+    ogTitle: "Savannah's Floatin' Party Starts Here | The Dancin Dolphin Cruise Company",
+  })
+</script>
+
 <template>
   <div>
     <h1>Test Test</h1>
